de4847e7b7f35763f2fa309da5fe6343c5066dd6
Brian Hasal <brian.hasal@gmail.com>
2022-10-15 18:01:36 -0700

    Added factorials

---
+++ quick_factorials.js
@@ -0,0 +1,40 @@
+/*
+  Make a function that computes a factorial recursively.
+  A factorial is when you take a number n and multiply by each preceding integer until you hit one.
+  n * (n-1) * (n-2) ... * 3 * 2 * 1
+  
+  Call the function factorial
+  
+  factorial(1) = 1
+  factorial(2) = 2
+  factorial(3) = 6 
+*/
+
+function factorial(n) {
+  let total = 1;
+  if (n === 1) {
+    return total * 1;
+  } else if (n === 2) {
+    return total * 2;
+  } else {
+    if ((n - 1) >= 2) {
+      total *= (n * factorial(n - 1));
+    }
+  }
+}
+
+// VS SOLUTION
+
+function factorial(num) {
+  if (num < 2) return 1;
+  return num * factorial(num - 1);
+}
+
+// unit tests
+// do not modify the below code
+test.skip("factorials", () => {
+  expect(factorial(1)).toEqual(1);
+  expect(factorial(2)).toEqual(2);
+  expect(factorial(3)).toEqual(6);
+  expect(factorial(10)).toEqual(3628800);
+});
